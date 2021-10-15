7c0c3056b2879445c681256d348c10462c5806cf
Marcel Kapfer <opensource@mmk2410.org>
2021-10-15 18:03:50 +0000

    Target impish

--- update-new-version.sh
+++ update-new-version.sh
@@ -3,7 +3,7 @@
 # This script intends to decrease the effort of updating the package.
 
 PACKAGE="intellij-idea-ultimate"
-DISTRIBUTION="hirsute"
+DISTRIBUTION="impish"
 
 main() {
     last_tag=$(git describe --abbrev=0 --tags)
