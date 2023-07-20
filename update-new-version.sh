dee8f31c89493647476e05aa6847ab77c727871a
Marcel Kapfer <opensource@mmk2410.org>
2023-07-20 17:47:15 +0200

    ✨ Further preparations for automatic package updates

--- update-new-version.sh
+++ update-new-version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # This script intends to decrease the effort of updating the package.
+#
+# Passing "--autoupdate" as first argument triggers automatic Git operations.
 
 set -euo pipefail
 
@@ -16,7 +18,15 @@ DISTRIBUTION="lunar"
 
 last_tag=$(git describe --abbrev=0 --tags)
 old="${last_tag#?}"
-new="$1"
+
+if [ "$1" = "--autoupdate" ]; then
+    new="$2"
+    autoupdate="y"
+else
+    new="$1"
+    autoupdate="n"
+fi
+
 name="$(git config --get user.name)"
 email="$(git config --get user.email)"
 
@@ -41,3 +51,17 @@ debuild -us -uc
 cd ..
 
 rm "$PACKAGE"_"$old"-*
+
+if [ "$autoupdate" = "n" ]; then
+    exit
+fi
+
+git add -A
+git commit -m "Upstream version $new"
+
+git switch main
+git merge --ff-only version-"$new"
+git push origin main
+
+git tag -a v"$new" "Upstream version $new"
+git push --tags origin main
